8fe93025ab5d670259964b1520bd920f129fa0e4
tomjackspencer <tspencer@alvarezandmarsal.com>
2026-06-16 21:25:28 +0100

    Delete essays.11tydata.js

--- essays.11tydata.js
+++
@@ -1,9 +0,0 @@
-// Shared front matter for everything in /essays/.
-// Each file is tagged "essay"; set `type: casenote` in a file to make it a case note.
-module.exports = {
-  layout: "essay.njk",
-  tags: ["essay"],
-  eleventyComputed: {
-    section: (data) => (data.type === "casenote" ? "casenotes" : "essays")
-  }
-};
